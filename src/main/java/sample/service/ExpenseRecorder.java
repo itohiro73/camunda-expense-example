3d67f8c4628b2630bdf346b2ccdcde49ec04be9b
Hiroshi Ito <itohiro73@gmail.com>
2018-01-15 19:15:57 +0900

    Implement working expense application.

---
+++ src/main/java/sample/service/ExpenseRecorder.java
@@ -0,0 +1,19 @@
+package sample.service;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.logging.Logger;
+
+public class ExpenseRecorder implements JavaDelegate {
+    private final Logger LOGGER = Logger.getLogger(ExpenseRecorder.class.getName());
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+
+        LOGGER.info("内容: " + execution.getVariable("detail"));
+        LOGGER.info("金額（円）: " + execution.getVariable("amount"));
+        LOGGER.info("経費種別: " + execution.getVariable("expenseCategory"));
+        LOGGER.info("承認済み？: " + (((Boolean)execution.getVariable("approved")).booleanValue() ? "はい" : "いいえ"));
+    }
+}
